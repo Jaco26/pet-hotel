3834a19b6da7dd8950b8b2c1f9b9622032713040
Zygy93 <amazinzygy@gmail.com>
2018-02-20 14:33:28 -0600

    added get all pets

--- server/routes/petRouter.js
+++ server/routes/petRouter.js
@@ -4,7 +4,7 @@ const pool = require('../modules/pool.js');
 
 router.post('/', function(req, res) {
     const addedOwner = req.body;
-    const sqlText = `INSERT INTO owners (first_name, last_name) 
+    const sqlText = `INSERT INTO owners (first_name, last_name)
     VALUES ($1, $2);`;
     pool.query(sqlText, [addedOwner.first_name, addedOwner.last_name])
     .then(function(response) {
@@ -12,11 +12,11 @@ router.post('/', function(req, res) {
         console.log(response);
     }).catch(function(error) {
         console.log(error);
-        res.sendStatus(500); 
+        res.sendStatus(500);
     }); // END pool.query
 }); // END router /pet_hotel POST
 
-router.get('/', function(req, res) {
+router.get('/owners', function(req, res) {
     const sqlText = `SELECT * FROM owners ORDER BY id LIMIT 50`;
     pool.query(sqlText)
     .then(function(response) {
@@ -24,14 +24,14 @@ router.get('/', function(req, res) {
         console.log(response);
     }).catch(function(error) {
         console.log(error);
-        res.sendStatus(500); 
+        res.sendStatus(500);
     }); // END pool.query
 }); // END router /pet_hotel GET
 
 router.post('/add', function(req, res){
     const petToAdd = req.body;
     console.log('petToAdd:', petToAdd);
-    
+
     const sqlText = `INSERT INTO pets (name, color, breed, owner_id) VALUES ($1, $2, $3, $4)`;
     pool.query(sqlText,[petToAdd.name, petToAdd.color, petToAdd.breed, petToAdd.owner_id])
     .then(function(response){
@@ -42,7 +42,17 @@ router.post('/add', function(req, res){
         console.log('error on add pet post:',error);
     })
 })//End router /add POST
-
+router.get('/pets', function(req, res) {
+    const sqlText = `SELECT * FROM pets JOIN owners ON pets.owner_id = owners.id ORDER BY pets.id LIMIT 50;`;
+    pool.query(sqlText)
+    .then(function(response) {
+        res.send(response.rows);
+        console.log(response);
+    }).catch(function(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }); // END pool.query
+}); // END router /pet_hotel GET
 
 
 

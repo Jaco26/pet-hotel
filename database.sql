1676ae49caef6010976624663656116ab4428c68
Jacob Albright <jacob.albright23@gmail.com>
2018-02-20 13:08:35 -0600

    added database.sql

---
+++ database.sql
@@ -0,0 +1,20 @@
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR,
+	last_name VARCHAR
+);
+
+CREATE TABLE pets (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR,
+	breed VARCHAR,
+	color VARCHAR,
+	owner_id INT references owners
+);
+
+CREATE TABLE visits (
+	id SERIAL PRIMARY KEY,
+	check_in DATE,
+	check_out DATE,
+	pet_id INT references pets
+);

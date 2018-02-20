19218c1c30d967f3bc751aec1573878603507ec4
Jacob Albright <jacob.albright23@gmail.com>
2018-02-20 11:55:28 -0600

    post is working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
     console.log('in jQ');
+    // getAppInfo()
+    
+    $('#registerBtn').on('click', function(e){
+
+        e.preventDefault();
+        packageOwner();
+    }); // END registrationBtn onclick
+
     
 }); // END document.ready
+
+// package owner info
+function packageOwner(){
+    let owner = {
+        first_name: $('#firstNameIn').val(),
+        last_name: $('#lastNameIn').val(),
+    }
+    sendOwner(owner);
+}
+
+
+// POST owner info
+function sendOwner(owner) {
+    console.log('in sendOwner');
+    
+    $.ajax({
+        type: 'POST',
+        url: '/pets',
+        data: owner
+    }).done(function(response) {
+        console.log(response);
+        // getInfo()
+    }).fail(function(error) {
+        console.log(error);
+    }); // END ajax POST
+}; // END sendOwner
--- server/routes/petRouter.js
+++ server/routes/petRouter.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
 const pool = require('../modules/pool.js');
 
-
+router.post('/', function(req, res) {
+    const addedOwner = req.body;
+    const sqlText = `INSERT INTO owners (first_name, last_name) 
+    VALUES ($1, $2);`;
+    pool.query(sqlText, [addedOwner.first_name, addedOwner.last_name])
+    .then(function(response) {
+        res.sendStatus(201);
+        console.log(response);
+    }).catch(function(error) {
+        console.log(error);
+        res.sendStatus(500); 
+    }); // END pool.query
+}); // END router /pets POST
 
 
 
